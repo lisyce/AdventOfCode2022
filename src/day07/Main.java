68e4b264dbe58ee7d8b3077cf1a3acf88ba854be
lisyce <lisyce@gmail.com>
2022-12-05 22:03:39 -0800

    template.bat for easier setup

---
+++ src/day07/Main.java
@@ -0,0 +1,13 @@
+package day07; 
+ 
+import java.io.File; 
+import java.io.FileNotFoundException; 
+import java.util.*; 
+ 
+public class Main { 
+   public static void main(String[] args) throws FileNotFoundException { 
+       File input = new File("src/day07/test.txt"); 
+       Scanner s = new Scanner(input); 
+ 
+   } 
+} 
---
+++ template.bat
@@ -0,0 +1,25 @@
+@echo off
+cd src
+
+set x=day%1%
+mkdir %x%
+cd %x%
+
+call >input.txt
+call > test.txt
+
+echo package %x%; > Main.java
+echo: >> Main.java
+echo import java.io.File; >> Main.java
+echo import java.io.FileNotFoundException; >> Main.java
+echo import java.util.*; >> Main.java
+echo: >> Main.java
+echo public class Main { >> Main.java
+echo    public static void main(String[] args) throws FileNotFoundException { >> Main.java
+echo        File input = new File("src/%x%/test.txt"); >> Main.java
+echo        Scanner s = new Scanner(input); >> Main.java
+echo: >> Main.java
+echo    } >> Main.java
+echo } >> Main.java
+
+cd ../..
