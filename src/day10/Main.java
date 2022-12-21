bf7a94672c897ae8e0b5ef7c902f139d6bf9dfec
lisyce <lisyce@gmail.com>
2022-12-21 13:16:46 -0800

    day 10 pt 2

--- src/day10/Main.java
+++ src/day10/Main.java
@@ -2,8 +2,7 @@ package day10;
  
 import java.io.File; 
 import java.io.FileNotFoundException; 
-import java.util.*; 
- 
+import java.util.*;
 public class Main { 
     public static void main(String[] args) throws FileNotFoundException { 
         File input = new File("src/day10/input.txt");
@@ -17,7 +16,13 @@ public class Main {
         int nextBenchmark = 20;
         int pt1SignalStrength = 0;
 
-        for (int i = 1; i <= 220; i++) {
+        for (int i = 1; i <= 240; i++) {
+            int cursorX = i % 40 == 0 ? 40 : i % 40;
+
+            if (cursorX >= x && cursorX < x + 3) {
+                System.out.print('#');
+            } else System.out.print(' ');
+
             if (i == nextBenchmark) {
                 pt1SignalStrength += nextBenchmark * x;
                 nextBenchmark += 40;
@@ -34,8 +39,11 @@ public class Main {
                 }
             }
 
+            if (i % 40 == 0) System.out.println();
         }
+        System.out.println();
         System.out.println("Part 1: " + pt1SignalStrength);
 
-    } 
+    }
+
 } 
