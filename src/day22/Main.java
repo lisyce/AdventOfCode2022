d790a419baf216b1e2785c8b8b760cdcf480f335
lisyce <lisyce@gmail.com>
2022-12-22 16:54:19 -0800

    savepoint

---
+++ src/day22/Main.java
@@ -0,0 +1,163 @@
+package day22; 
+ 
+import java.io.File; 
+import java.io.FileNotFoundException; 
+import java.util.*;
+
+
+public class Main { 
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File input = new File("src/day22/input.txt");
+        Scanner s = new Scanner(input);
+
+        // build grid
+        List<Row> grid = buildGrid(s);
+
+        // follow directions
+        int x = grid.get(0).firstOpenTileX();
+        int y = 1;
+        int xDir = 1;
+        int yDir = 0;
+
+        Scanner instructionScan = new Scanner(s.nextLine());
+        instructionScan.useDelimiter("[RL]");
+        // we can also use the \\d+ delim for nums. we just need to switch between them
+
+        while (instructionScan.hasNext()) {
+            String instruction = instructionScan.next();
+            System.out.println(instruction);
+
+            try {
+                int dist = Integer.parseInt(instruction);
+
+                if (xDir != 0) { // moving L or R
+                    for (int i = 0; i < dist; i++) {
+                        // is the next space clear?
+                        int nextSpace = grid.get(y - 1).nextXSpace(x, xDir);
+                        if (nextSpace == -1) break;
+
+                        // move in the x direction
+                        x = nextSpace;
+
+//                        System.out.println(x + ", " + y);
+
+                    }
+                } else { // moving U or D
+
+                    for (int i = 0; i < dist; i++) {
+                        // is the next space clear?
+                        int nextSpace = nextYSpace(grid, y, x, yDir);
+                        if (nextSpace == -1) break;
+
+                        // move in the y direction
+                        y = nextSpace;
+
+//                        System.out.println(x + ", " + y);
+
+                    }
+                }
+
+
+            } catch (NumberFormatException e) {
+
+                switch (instruction) {
+                    case "L":
+                        if (xDir != 0) {
+                            yDir = -xDir;
+                            xDir = 0;
+                        } else {
+                            xDir = yDir;
+                            yDir = 0;
+                        }
+                        break;
+                    case "R":
+                        if (xDir != 0) {
+                            yDir = xDir;
+                            xDir = 0;
+                        } else {
+                            xDir = -yDir;
+                            yDir = 0;
+                        }
+                        break;
+                }
+            }
+
+            if (instructionScan.delimiter().toString().equals("[RL]")) {
+                instructionScan.useDelimiter("\\d+");
+                System.out.println(x + ", " + y);
+                System.out.println();
+            } else {
+                instructionScan.useDelimiter("[RL]");
+
+            }
+        }
+
+        // figure out final password
+        int facingPoints = xDir == 1 ? 0 : xDir == -1 ? 2 : yDir == 1 ? 1 : 3;
+        int password = (1000 * y) + (4 * x) + facingPoints;
+        System.out.println("Part 1: " + password);
+    }
+
+    // returns -1 if the next space is blocked
+    public static int nextYSpace(List<Row> grid, int currY, int currX, int yDir) {
+
+//        // are we on the bottom edge of the grid and moving down?
+//        if (currY == grid.size() && yDir == 1) {
+//            Row topRow = grid.get(0);
+//            return topRow.spaceIsValid(currX) ? 1 : -1;
+//
+//        } else if (currY == 1 && yDir == -1) { // are we on the top edge of the grid and moving up?
+//            Row bottomRow = grid.get(grid.size() - 1);
+//            return bottomRow.spaceIsValid(currX) ? grid.size() - 1 : -1;
+
+//        }
+//        else  { // can we just move to the next spot?
+            Row nextRow = grid.get(currY - 1 + yDir);
+            if (nextRow.spaceIsValid(currX)) {
+                return currY + yDir;
+
+            } else if (grid.get(currY - 1 + yDir).wallX.contains(currX)) { // are we just blocked by a wall? if so, stop
+                return -1;
+            } else { // move in the opposite direction until we find the row to look for
+                while (true) {
+                    if (currY - 1 == 1 || currY == grid.size()) break;
+
+                    Row prevRow = grid.get(currY - yDir - 1);
+                    if (prevRow.startX > currX || prevRow.endX < currX) break;
+                    currY -= yDir;
+                }
+                return grid.get(currY - 1).spaceIsValid(currX) ? currY : -1;
+            }
+        }
+//    }
+
+    public static List<Row> buildGrid(Scanner s) {
+
+        // build grid
+        List<Row> grid = new ArrayList<>();
+
+        String line = s.nextLine();
+
+        do {
+            // build row
+            Row r = new Row();
+
+            for (int i = 0; i < line.length(); i++) {
+                char c = line.charAt(i);
+
+                if (c != ' ') {
+                    if (r.startX == 0) r.startX = i+1;
+
+                    if (c == '#') r.wallX.add(i+1);
+                }
+            }
+            r.endX = line.length();
+            grid.add(r);
+
+            line = s.nextLine();
+        } while (!line.isEmpty());
+
+        return grid;
+    }
+} 
---
+++ src/day22/Row.java
@@ -0,0 +1,49 @@
+package day22;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Row {
+
+    int startX;
+    int endX;
+    Set<Integer> wallX;
+
+    public Row() {
+        this.wallX = new TreeSet<>();
+    }
+
+    public int firstOpenTileX() {
+        for (int i = startX; i <= endX; i++) {
+            if (!wallX.contains(i)) return i;
+        }
+
+        return -1;
+    }
+
+    // returns whether the space is clear and within the bounds of this row
+    public boolean spaceIsValid(int x) {
+        return !wallX.contains(x) && startX <= x && endX >= x;
+    }
+
+    // returns -1 if the next space is not clear
+    public int nextXSpace(int currX, int dir) {
+        if (currX == startX && dir == -1) {
+            // wrap around
+            return !wallX.contains(endX) ? endX : -1;
+        } else if (currX == endX && dir == 1) {
+            return !wallX.contains(startX) ? startX : -1;
+        }
+
+        // just check the next space
+        return !wallX.contains(currX + dir) ? currX + dir : -1;
+    }
+
+    @Override
+    public String toString() {
+        return "start: " + startX + ", end: " + endX + ", walls: " + wallX;
+    }
+
+}
